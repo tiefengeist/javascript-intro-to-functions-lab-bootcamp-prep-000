60bd44fc1aedc310eb9d5d361a63ae5c21e35a62
Sahir Nambiar <sahirnambiar@gmail.com>
2018-10-26 23:28:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function whisper(string) {
   return string.toLowerCase();
 }
 
-function logShout(string) {
-  uppercase = string.toUpperCase();
+function logShout(string){
+  uppercase = string.toUpperCase()
   string = uppercase;
   console.log(uppercase);
 }
